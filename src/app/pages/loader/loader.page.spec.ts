28827afc098b80b5ddd998b316c10c3c8c0a7e90
zaykha <zaykha@gmail.com>
2022-03-15 18:16:17 +0630

    test and update loader page router

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,18 +1,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { LoaderPage } from './loader.page';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoaderPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(),
+      AppRoutingModule
+      ]
     }).compileComponents();
-
+    router = TestBed.get(Router);
     fixture = TestBed.createComponent(LoaderPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,4 +26,11 @@ describe('LoaderPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go to login page after load', ()=> {
+    spyOn(router, 'navigate');
+    component.ngOnInit();
+
+    expect(router.navigate).toHaveBeenCalledWith(['login'])
+  })
 });
